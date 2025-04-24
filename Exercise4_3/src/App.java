d7ec8c5c853cad66bbf06a5219e4e71ca8b9934b
DoubleDuy <ddoubleduy@gmail.com>
2025-04-24 09:08:34 +0700

    Delete Exercise4_3 directory

--- Exercise4_3/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        Point2D obj = new Point2D(3, 8);
-        System.out.println(obj.toString());
-
-        Point2D obj2 = new Point3D(9, 1, 2);
-        System.out.println(obj2.toString());
-    }
-}
