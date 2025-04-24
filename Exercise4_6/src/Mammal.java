a909709ae79eaf75ae0177f00c4892f6dc80a77c
DoubleDuy <ddoubleduy@gmail.com>
2025-04-24 09:08:47 +0700

    Delete Exercise4_6 directory

--- Exercise4_6/src/Mammal.java
+++
@@ -1,9 +0,0 @@
-public class Mammal extends Animal{
-    public Mammal(String name) {
-        super(name);
-    }
-
-    public String toString() {
-        return "Mammal["+super.toString()+"]";
-    }
-}
