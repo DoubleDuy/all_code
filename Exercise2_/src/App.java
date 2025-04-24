17ef6b0e13f5765332773ddbae0a9d0ab7fb48e4
DoubleDuy <ddoubleduy@gmail.com>
2025-04-24 08:52:29 +0700

    Add files via upload

---
+++ Exercise2_/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Employee i = new Employee(001, "Double", "Duy", 15000);
+        System.out.println("ID :" + i.getID());
+        System.out.println("NAME :" + i.getName());
+        System.out.println("Salary :" + i.getSalary());
+        System.out.println("AnnualSalary :" + i.getAnnualSalary());
+        System.out.println("RaiseSalary :" + i.raiseSalary(10));
+        System.out.println(i.toString());
+    }
+}
