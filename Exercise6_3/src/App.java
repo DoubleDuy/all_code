f3a358c186342c5422dd9b98a15b73ffbfe28aaa
DoubleDuy <ddoubleduy@gmail.com>
2025-04-24 09:09:01 +0700

    Delete Exercise6_3 directory

--- Exercise6_3/src/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        MovablePoint Point = new MovablePoint(5, 7, 10, 20);
-        System.out.println(Point.toString());
-    }
-}
--- Exercise6_3/src/Movable.java
+++
@@ -1,6 +0,0 @@
-interface Movable {
-    abstract void moveUp();
-    abstract void moveDown();
-    abstract void moveLeft();
-    abstract void moveRight();
-}
